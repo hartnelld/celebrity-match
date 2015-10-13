d9c6aa0ac4062b5f354a9ed1e2b4c7345d4599ba
Daniel Hartnell & Jeff Seymour <student@epicodus.com>
2015-10-12 20:07:23 -0700

    add initial site layout without brains.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 $(function() {
   $("#questions form").submit(function(event){
+    // Set variables based on form input
     var gender = $("#inputGender").val();
     var age = $("#inputAge").val();
-    alert(gender);
+    var skill = $("#inputSkill").val();
+
+    // Validate input
+
+    // Identify celebrity
+    if (gender === "Male") {
+      var celebrity = "Scarlett Johansson";
+    } else {
+      var celebrity = "George Clooney";
+    }
+
+    // Add content to celebrity div
+    $(".celebrity-match").text(celebrity);
+
+    // Hide #questions and show #results
+    $("#questions").hide();
+    $("#results").show();
+
+    // Prevent default action when submit event occurs
     event.preventDefault();
   });
 });
